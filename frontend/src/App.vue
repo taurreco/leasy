5783694ba284b300f5f01b1755a2218713e09610
Jared Dunn <jdunn@webucator.com>
2022-02-17 10:18:59 -0500

    feat: add nav and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,41 @@
 <template>
   <div id="app">
-    <h1>Hello App!</h1>
-    <p>
-      <router-link to="/">Go to Home</router-link>
-      <router-link to="/about">Go to About</router-link>
-    </p>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Leasy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#leasy-nav" aria-controls="leasy-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="leasy-nav">
+          <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+            <li class="nav-item">
+              <router-link to="/" custom v-slot="{ href, isActive }">
+                <a :href="href" class="nav-link" :class="{'active': isActive}">Home</a>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/listings" custom v-slot="{ href, isActive }">
+                <a :href="href" class="nav-link" :class="{'active': isActive}">Listings</a>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/contact" custom v-slot="{ href, isActive }">
+                <a :href="href" class="nav-link" :class="{'active': isActive}">Contact</a>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" custom v-slot="{ href, isActive }">
+                <a :href="href" class="nav-link" :class="{'active': isActive}"><span class="rounded bg-primary text-light p-2">Log in</span></a>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
+    <footer class="bg-dark container-fluid text-light position-absolute bottom-0">
+      <p class="text-center m-0 p-2">&copy; Leasy. All rights reserved.</p>
+    </footer>
   </div>
 </template>
 
