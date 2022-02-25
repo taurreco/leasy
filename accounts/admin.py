e72fddd0d755a11d4339fa12424144929bc45d5b
Jared Dunn <jdunn@webucator.com>
2022-02-25 17:49:50 -0500

    chore: add first_name, last_name to custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,12 +10,12 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = [
         "email",
-        "username",
-        "name",
+        "first_name",
+        "last_name",
         "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
