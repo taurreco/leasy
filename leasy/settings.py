910ed8b6378e5107db80cb246567a8a556299b44
Jared Dunn <jdunn@webucator.com>
2022-02-24 19:56:07 -0500

    chore: add apis app and rest_framework

--- leasy/settings.py
+++ leasy/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "rest_framework",
+    # local
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
@@ -59,7 +63,7 @@ ROOT_URLCONF = "leasy.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "frontend/build/templates"],
+        "DIRS": [BASE_DIR / "client/build/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "static/"
-STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [BASE_DIR / "client/build/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
