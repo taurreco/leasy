62d21597668be9569fdc6b14b6ff416b45849d96
Jared Dunn <jdunn@webucator.com>
2022-02-25 17:50:37 -0500

    feat: make email primary key

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class CustomUser(AbstractUser):
-    name = models.CharField(null=True, blank=True, max_length=100)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    # TODO figure out how to remove username as field
+    username = models.CharField(unique=False, max_length=100)
+    email = models.EmailField(("email address"), unique=True)
+    # TODO add unique phone number as well?
