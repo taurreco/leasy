b42ad853130dd0b7eb1445f624615246884d68a0
Jared Dunn <jdunn@webucator.com>
2022-02-25 14:00:55 -0500

    feat: add user api views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
