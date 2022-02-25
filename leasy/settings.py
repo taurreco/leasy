faefa2070c7ea128930d2cb1fdf8018266dace7e
Jared Dunn <jdunn@webucator.com>
2022-02-25 11:24:50 -0500

    refactor: rename frontend to client

--- leasy/settings.py
+++ leasy/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "leasy.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "frontend/build/templates"],
+        "DIRS": [BASE_DIR / "client/build/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "static/"
-STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [BASE_DIR / "client/build/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
