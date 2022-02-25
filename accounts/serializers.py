cd0afcf9b6d72447b161e782f728da8cd144aa29
Jared Dunn <jdunn@webucator.com>
2022-02-25 17:47:27 -0500

    feat: create custom login/register serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,22 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from dj_rest_auth.serializers import LoginSerializer
+from dj_rest_auth.registration.serializers import RegisterSerializer
+
+
+class CustomLoginSerializer(LoginSerializer):
+    username = None
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    username = None
+    # email = serializers.EmailField(required=True)
+
+    def get_cleaned_data(self):
+        return {
+            "email": self.validated_data.get("email", ""),
+            "password1": self.validated_data.get("password1", ""),
+        }
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,5 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = (
             "id",
-            "username",
+            "first_name",
+            "last_name",
+            "email",
         )
