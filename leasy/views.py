6f847449326a4a4a0a42e6d9a1a4ea06bb753647
Jared Dunn <jdunn@webucator.com>
2022-02-26 10:57:03 -0500

    feat: add confirm_email_view

--- leasy/views.py
+++ leasy/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from .settings import ROOT_URL
+
+import requests
 
 # Create your views here.
 def frontend(request, resource):
@@ -8,4 +12,20 @@ def frontend(request, resource):
 
 def api_redirect(request):
     response = redirect("/api/v1/docs/")
+    return response
+
+
+def confirm_email_view(request, key):
+    """Confirms a user's email and redirects them to the homepage
+
+    Args:
+        request (HttpRequest): the request made to this view
+        key (String): the verification key to confirm the email
+
+    Returns:
+        HttpResponse: a redirect to the homepage
+    """
+    url = ROOT_URL + reverse("rest_verify_email")
+    requests.post(url, data={"key": key})
+    response = redirect("/")
     return response
