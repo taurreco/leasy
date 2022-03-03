ba9b7e1d06804b0308454251a1886abca2dc0fef
Jared Dunn <jdunn@webucator.com>
2022-03-03 10:54:47 -0500

    refactor: add user auth urls to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,49 @@
-from django.urls import path
-from rest_framework.routers import SimpleRouter
+from django.urls import path, re_path, include
+from dj_rest_auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+)
+from dj_rest_auth.registration.views import (
+    RegisterView,
+    ResendEmailVerificationView,
+    VerifyEmailView,
+)
 
-from .views import UserViewSet
+from .views import confirm_email_view
 
 
-router = SimpleRouter()
-router.register("", UserViewSet, basename="users")
+user_auth_urls = [
+    # dj-rest-auth endpoints
+    # https://dj-rest-auth.readthedocs.io/en/latest/api_endpoints.html
+    path("password/change/", PasswordChangeView.as_view(), name="rest_password_change"),
+    path("password/reset/", PasswordResetView.as_view(), name="rest_password_reset"),
+    path(
+        "password/reset/confirm/",
+        PasswordResetConfirmView.as_view(),
+        name="rest_password_reset_confirm",
+    ),
+    path("login/", LoginView.as_view(), name="rest_login"),
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
+    # dj-rest-auth registration endpoints
+    # https://dj-rest-auth.readthedocs.io/en/latest/api_endpoints.html#registration
+    path("register/", RegisterView.as_view(), name="rest_register"),
+    path(
+        "resend-email/", ResendEmailVerificationView.as_view(), name="rest_resend_email"
+    ),
+    path(
+        "account-email-verification-sent/",
+        VerifyEmailView.as_view(),
+        name="account_email_verification_sent",
+    ),
+    re_path(
+        r"^account-confirm-email/(?P<key>[-:\w]+)/$",
+        confirm_email_view,
+        name="account_confirm_email",
+    ),
+]
 
-urlpatterns = router.urls
+app_name = "accounts"
+urlpatterns = [path("", include(user_auth_urls))]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,23 @@
-from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from django.shortcuts import redirect
+from django.urls import reverse
 
-from .serializers import UserSerializer
+import requests
+
+from leasy.settings import ROOT_URL
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+def confirm_email_view(request, key):
+    """Confirms a user's email and redirects them to the homepage
+
+    Args:
+        request (HttpRequest): the request made to this view
+        key (String): the verification key to confirm the email
+
+    Returns:
+        HttpResponse: a redirect to the login page
+    """
+    confirm_email_url = ROOT_URL + reverse("accounts:account_email_verification_sent")
+    requests.post(confirm_email_url, data={"key": key})
+
+    response = redirect("login")
+    return response
