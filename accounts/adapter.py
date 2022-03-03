91030b49d9a54d0cc7ea9998585960b23fd5c4ca
Jared Dunn <jdunn@webucator.com>
2022-03-03 10:52:50 -0500

    feat: custom adapter for custom email confirmation

---
+++ accounts/adapter.py
@@ -0,0 +1,20 @@
+from django.urls import reverse
+from django.http.response import HttpResponseRedirect
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.utils import build_absolute_uri
+
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def get_email_confirmation_url(self, request, emailconfirmation):
+        """Constructs the email confirmation (activation) url.
+
+        Note that if you have architected your system such that email
+        confirmations are sent outside of the request context `request`
+        can be `None` here.
+        """
+        url = reverse("accounts:account_confirm_email", args=[emailconfirmation.key])
+        ret = build_absolute_uri(request, url)
+        return ret
+
+    def respond_email_verification_sent(self, request, user):
+        return HttpResponseRedirect(reverse("accounts:account_email_verification_sent"))
