26172b986ec91ae8306f64acbb20e39e78a5ccd8
taurreco <tomasdougan@gmail.com>
2022-04-19 11:03:20 -0400

    added listing urls

---
+++ listings/urls.py
@@ -0,0 +1,16 @@
+
+# listings/urls.py
+
+from django.urls import path, include
+
+from .views import ListingsViewSet, listings_endpoints
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register("listings", ListingsViewSet, basename="listings")
+
+app_name="listings"
+urlpatterns = [
+    path("", include(router.urls)),
+    path("endpoints/", listings_endpoints),
+]
