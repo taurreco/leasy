3de083fc63c4bccd8a29c182b856f903e2b24170
taurreco <tomasdougan@gmail.com>
2022-04-19 11:23:07 -0400

    added listings vue methods

--- frontend/src/views/Listings.vue
+++ frontend/src/views/Listings.vue
@@ -1,10 +1,25 @@
 <template>
   <h1>Listings</h1>
+  <div v-for="listing in getListings">
+    {{listing.name}}
+  </div>
 </template>
 
 <script>
+import {mapGetters, mapActions} from 'vuex';
+
 export default {
   name: 'Listings',
+  async mounted() {
+    await this.loadEndpoints();
+    await this.loadListings();
+  },
+  computed: {
+    ...mapGetters("listings", ["getListings"])
+  },
+  methods: {
+    ...mapActions("listings", ["loadEndpoints", "loadListings"])
+  }
 }
 </script>
 
