c2113e31cf8c8bcaad5161d8f949daac2c8eb222
taurreco <tomasdougan@gmail.com>
2022-04-19 11:02:26 -0400

    added listing serializer

---
+++ listings/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Listing
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            "name",
+            "address",
+            "rent",
+            "move_in",
+            "move_out",
+            "desc",
+        ]
