c449bf9eb972ed3b5dcd4171f72e4f77f7cd87eb
taurreco <tomasdougan@gmail.com>
2022-04-19 11:01:50 -0400

    added listing view

---
+++ listings/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse
+
+from listings.serializers import ListingSerializer
+from .models import Listing
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+
+class ListingsView(ListView):
+    model = Listing
+    template_name = "listings.html"
+
+class ListingsViewSet(ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+@api_view()
+def listings_endpoints(request):
+    endpoints = {
+        "listings-list": reverse("listings:listings-list"),
+    }
+
+    return Response(endpoints)
+
