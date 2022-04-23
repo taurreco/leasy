ef4abe332c40ec64264424e814501ba1ce345f59
Jared Dunn <jdunn@webucator.com>
2022-04-23 17:03:18 -0400

    feat: show more properties on listings page

--- frontend/src/views/Listings.vue
+++ frontend/src/views/Listings.vue
@@ -1,10 +1,11 @@
 <template>
   <h1>Listings</h1>
-  <div v-for="listing in getListings" :key="listing.id">
-    <h2>
-      {{listing.name}}
-    </h2>
-    <p>{{listing.desc}}</p>
+  <div v-for="listing in getListings" :key="listing.id" class="border">
+    <h2>{{listing.name}}</h2>
+    <p>{{listing.lister_first_name}}</p>
+    <p>{{listing.address}}</p>
+    <p>{{listing.description}}</p>
+    <p>${{listing.rent}}/month</p>
   </div>
 </template>
 
