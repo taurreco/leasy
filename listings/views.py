35dece46d7d139c70541e4186391111103251fad
Jared Dunn <jdunn@webucator.com>
2022-04-23 17:07:01 -0400

    chore: remove ListingsView

--- listings/views.py
+++ listings/views.py
@@ -6,11 +6,6 @@ from .models import Listing
 from rest_framework.viewsets import ModelViewSet
 
 
-class ListingsView(ListView):
-    model = Listing
-    template_name = "listings.html"
-
-
 class ListingsViewSet(ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
