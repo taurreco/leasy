065595affb0dc5fa6c0a45beaaab8986c48a019e
Jared Dunn <jdunn@webucator.com>
2022-04-23 15:38:24 -0400

    feat: add images to associate with listing

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
-from .models import Listing
+from .models import Listing, ListingImage
 
 from accounts.models import CustomUser
+
+
+class ListingImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListingImage
+        fields = ["description", "image"]
 
 
 class ListingSerializer(serializers.ModelSerializer):
     lister = serializers.PrimaryKeyRelatedField(
         queryset=CustomUser.objects.all(),
     )
+    images = ListingImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Listing
-        fields = ["name", "address", "rent", "description", "lister", "id"]
+        fields = ["name", "address", "rent", "description", "lister", "id", "images"]
