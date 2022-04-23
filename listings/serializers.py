d0f790da2f2941582a02047d8f2e473c6b03c7e7
Jared Dunn <jdunn@webucator.com>
2022-04-23 17:03:01 -0400

    feat: add lister first name to serializer

--- listings/serializers.py
+++ listings/serializers.py
@@ -20,6 +20,7 @@ class ListingSerializer(serializers.ModelSerializer):
     lister = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
     images = ListingImageSerializer(many=True, read_only=True)
     bookings = ListingBookingSerializer(many=True, read_only=True)
+    lister_first_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Listing
@@ -30,8 +31,17 @@ class ListingSerializer(serializers.ModelSerializer):
             "rent",
             "description",
             "lister",
+            "lister_first_name",
             "id",
             "images",
             "start_date",
             "end_date",
         ]
+
+    def get_lister_first_name(self, listing):
+        """Gets the first name of the lister if there is a first name."""
+        lister = listing.lister
+        if hasattr(lister, "first_name"):
+            return lister.first_name
+
+        return None
