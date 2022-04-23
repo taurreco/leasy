ec2b5ce084935ea0f370f0eeb4da4d054cdff31c
Jared Dunn <jdunn@webucator.com>
2022-04-23 12:41:10 -0400

    refactor: make lister required

--- listings/models.py
+++ listings/models.py
@@ -13,7 +13,7 @@ class Listing(models.Model):
     address = models.CharField(max_length=250)
     rent = models.IntegerField()
     description = models.CharField(max_length=40000)
-    lister = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    lister = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.name
